5d5b59b29dd8896ee87ce4f1304abb1e16d6bb8d
Elizabeth Bartholomew <lizzy.bartholomew@gmail.com>
2023-09-01 00:48:02 -0600

    updated

---
+++ notes.md
@@ -0,0 +1,31 @@
+[8:42AM]
+Okay, it's official. I have no idea what I did to make the radio buttons work. I've done this several times, but for some reason I can't get it to work for this program.
+
+I have a memory of changing one small little thing, and then it working like magic, but I can't remember or find what that thing was. 
+
+[10:54AM]
+Seriously? Okay, so I finally caved and googled it. Apparently, if you only want one radio button selected at a time, all you have to do is give them all the same name. 
+
+But I was right about how just changing oen small thing got it to work.
+
+So that's that problem solved. 
+
+[00:02:00]
+Okay, the next problem I need to solve is to access the data from the data.json file. I'm still not super familiar with JavaScript, but it looks like some sort of dictionary. I can look up a Youtube video. 
+
+[00:37:00]
+I would save a lot of time on these programming projects if I actually thought them through all the way. I always THINK I understand how to do it, but then I spend half my time undoing something or going back because I misunderstood or realized there was a better way to do it or that I was just plain doing it wrong. It's frustrating beyond belief.
+
+But how do I plan out a project in a way that will forsee these unforseen problems before they happen? Reading the entire description would probably help. And there are probably tons of Youtube videos about how to properly plan out a project. Half of them will be all about the stupid obvious stuff. But I should probably give it a look anyway. 
+
+[01:01:00]
+Well, I'm doing something wrong. So I watched a video on how to work with JSON files. It's pretty simple really. All I needed was the commmand. The only problem is, the video didn't go over what the command would be if the dictionary included an object type. So I don't know how to phrase that part of the command, which is probably why it's not working. 
+
+[01:07:00]
+So google isn't really giving me the exact answer I want. Off to ChatGPT I go.
+
+[01:10:00]
+That wasn't really helpful. ChatGPT had an interesting solution of basically looping through the data until it found the right one. But as this is a dictionary, I should just be able to access it via the title. That's the whole point of a dictionary. 
+
+[01:20:00]
+Okay, it's partially working. The problem was I wasn't going doing the tree properly. But it only works on the work square and the play square, and the "previous" portion comes up as undefined. I'm not sure what's that about.
--- script.js
+++ script.js
@@ -1,13 +1,192 @@
-function timeChoice(){
-    if(document.getElementById("daily").checked){
-        document.getElementById("daily-label").style.color = 'white';
+function dataCrap(){
+
+    let dailyButton = document.getElementById("daily");
+    let weeklyButton = document.getElementById("weekly");
+    let monthlyButton = document.getElementById("monthly");
+
+    let workHours = document.getElementById("work-hours");
+    let workSummary = document.getElementById("work-summary");
+
+    let playHours = document.getElementById("play-hours");
+    let playSummary = document.getElementById("play-summary");
+
+    let exerciseHours = document.getElementById("exericse-hours");
+    let exerciseSummary = document.getElementById("exercise-summary");
+
+    let socialHours = document.getElementById("social-hours");
+    let socialSummary = document.getElementById("social-summary");
+
+    let selfHours = document.getElementById("self-hours");
+    let selfSummary = document.getElementById("self-summary");
+
+    let info = `[
+        {
+          "title": "Work",
+          "timeframes": {
+            "daily": {
+              "current": 5,
+              "previous": 7
+            },
+            "weekly": {
+              "current": 32,
+              "previous": 36
+            },
+            "monthly": {
+              "current": 103,
+              "previous": 128
+            }
+          }
+        },
+        {
+          "title": "Play",
+          "timeframes": {
+            "daily": {
+              "current": 1,
+              "previous": 2
+            },
+            "weekly": {
+              "current": 10,
+              "previous": 8
+            },
+            "monthly": {
+              "current": 23,
+              "previous": 29
+            }
+          }
+        },
+        {
+          "title": "Study",
+          "timeframes": {
+            "daily": {
+              "current": 0,
+              "previous": 1
+            },
+            "weekly": {
+              "current": 4,
+              "previous": 7
+            },
+            "monthly": {
+              "current": 13,
+              "previous": 19
+            }
+          }
+        },
+        {
+          "title": "Exercise",
+          "timeframes": {
+            "daily": {
+              "current": 1,
+              "previous": 1
+            },
+            "weekly": {
+              "current": 4,
+              "previous": 5
+            },
+            "monthly": {
+              "current": 11,
+              "previous": 18
+            }
+          }
+        },
+        {
+          "title": "Social",
+          "timeframes": {
+            "daily": {
+              "current": 1,
+              "previous": 3
+            },
+            "weekly": {
+              "current": 5,
+              "previous": 10
+            },
+            "monthly": {
+              "current": 21,
+              "previous": 23
+            }
+          }
+        },
+        {
+          "title": "Self Care",
+          "timeframes": {
+            "daily": {
+              "current": 0,
+              "previous": 1
+            },
+            "weekly": {
+              "current": 2,
+              "previous": 2
+            },
+            "monthly": {
+              "current": 7,
+              "previous": 11
+            }
+          }
+        }
+    ]`
+
+    if (dailyButton.checked = true){
+        // some crap here
+        workHours.innerHTML = JSON.parse(info)[0].timeframes.daily.current;
+        workSummary.innerHTML = JSON.parse(info)[0].timeframes.daily.pervious;
+        
+        playHours.innerHTML = JSON.parse(info)[1].timeframes.daily.current;
+        playSummary.innerHTML = JSON.parse(info)[1].timeframes.daily.pervious;
+
+        studyHours.innerHTML = JSON.parse(info)[2].timeframes.daily.current;
+        studySummary.innerHTML = JSON.parse(info)[2].timeframes.daily.pervious;
+
+        exerciseHours.innerHTML = JSON.parse(info)[3].timeframes.daily.current;
+        exerciseSummary.innerHTML = JSON.parse(info)[3].timeframes.daily.pervious;
+
+        socialHours.innerHTML = JSON.parse(info)[4].timeframes.daily.current;
+        socialSummary.innerHTML = JSON.parse(info)[4].timeframes.daily.pervious;
+
+        selfHours.innerHTML = JSON.parse(info)[5].timeframes.daily.current;
+        selfSummary.innerHTML = JSON.parse(info)[5].timeframes.daily.pervious;
     }
 
-    if(document.getElementById("weekly").checked){
-        document.getElementById("weekly-label").style.color = "white";
+    if (weeklyButton.checked = true){
+        // some crap here
+        workHours.innerHTML = JSON.parse(info)[0].timeframes.weekly.current;
+        workSummary.innerHTML = JSON.parse(info)[0].timeframes.weekly.pervious;
+        
+        playHours.innerHTML = JSON.parse(info)[1].timeframes.weekly.current;
+        playSummary.innerHTML = JSON.parse(info)[1].timeframes.weekly.pervious;
+
+        studyHours.innerHTML = JSON.parse(info)[2].timeframes.weekly.current;
+        studySummary.innerHTML = JSON.parse(info)[2].timeframes.weekly.pervious;
+
+        exerciseHours.innerHTML = JSON.parse(info)[3].timeframes.weekly.current;
+        exerciseSummary.innerHTML = JSON.parse(info)[3].timeframes.weekly.pervious;
+
+        socialHours.innerHTML = JSON.parse(info)[4].timeframes.weekly.current;
+        socialSummary.innerHTML = JSON.parse(info)[4].timeframes.weekly.pervious;
+
+        selfHours.innerHTML = JSON.parse(info)[5].timeframes.weekly.current;
+        selfSummary.innerHTML = JSON.parse(info)[5].timeframes.weekly.pervious;
     }
 
-    if(document.getElementById("monthly").checked){
-        document.getElementById("monthly-label").style.color = "white";
+    if (Button.checked = true){
+        // some crap here
+        workHours.innerHTML = JSON.parse(info)[0].timeframes.monthly.current;
+        workSummary.innerHTML = JSON.parse(info)[0].timeframes.monthly.pervious;
+        
+        playHours.innerHTML = JSON.parse(info)[1].timeframes.monthly.current;
+        playSummary.innerHTML = JSON.parse(info)[1].timeframes.monthly.pervious;
+
+        studyHours.innerHTML = JSON.parse(info)[2].timeframes.monthly.current;
+        studySummary.innerHTML = JSON.parse(info)[2].timeframes.monthly.pervious;
+
+        exerciseHours.innerHTML = JSON.parse(info)[3].timeframes.monthly.current;
+        exerciseSummary.innerHTML = JSON.parse(info)[3].timeframes.monthly.pervious;
+
+        socialHours.innerHTML = JSON.parse(info)[4].timeframes.monthly.current;
+        socialSummary.innerHTML = JSON.parse(info)[4].timeframes.monthly.pervious;
+
+        selfHours.innerHTML = JSON.parse(info)[5].timeframes.monthly.current;
+        selfSummary.innerHTML = JSON.parse(info)[5].timeframes.monthly.pervious;
     }
+    
+    
+    
 }
